dc66e4162ea7c2b26a5230f0d1d07d52928e7528
Mohammad Abudarda Khan <mohkhan34@publicisgroupe.net>
2022-04-27 17:13:34 +0530

    project pushed

---
+++ src/test/java/com/sapient/endure/assessment1/AppTest.java
@@ -0,0 +1,14 @@
+package com.sapient.endure.assessment1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
